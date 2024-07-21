aafed5d12c20a01cb0aed6bb9531e176eb0d3488
indekwe <findekwe@gmail.com>
2024-07-21 14:17:23 -0700

    commit on handling local storage of to do list

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function (event){
     const taskInput =document.getElementById('task-input')
     const taskList=document.getElementById('task-list')
     //addition of tasks to task list
-    function addTask (){
-        const taskText= taskInput.value.trim()
+    function addTask (taskText, save = true){
+        taskText= taskInput.value.trim()
         if(taskText.length<=0){
             alert('No task added')
         }
@@ -16,14 +16,25 @@ document.addEventListener('DOMContentLoaded', function (event){
             li.textContent=taskText;
             const removeButton=document.createElement('button')
             removeButton.textContent='Remove'
-            removeButton.classList.add('remove-btn')
-            removeButton.onclick=()=>{taskList.removeChild(li)}
+            removeButton.classList.add='remove-btn'
+            removeButton.onclick=()=>{
+            taskList.removeChild(li)
+            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+            const indexToRemove=storedTasks.findIndex(task=>task===li.textContent);
+            storedTasks.splice(indexToRemove,1);
+            localStorage.setItem('tasks', JSON.stringify(storedTasks));
+            }
             li.appendChild(removeButton)
             taskList.appendChild(li)
             taskInput.value=''
             
         }
-            
+        //adding new task to the local storage
+        if (save) {
+            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+            storedTasks.push(taskText);
+            localStorage.setItem('tasks', JSON.stringify(storedTasks));
+        }  
     }
     //attaching events listeners to ensure interactivity
             addButton.addEventListener('click', addTask)
@@ -34,8 +45,10 @@ document.addEventListener('DOMContentLoaded', function (event){
             })
              //ensuring data fetching to run after html loaded fully
              document.addEventListener('DOMContentLoaded', addTask)
-    
+    //checking presence of tasks in local storage
+    function loadTasks() {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.forEach(taskText => addTask(taskText, false))
+    }
     
 })
-const li=document.createElement('li')
-console.log(li)
